3b4f3b1827c0c6efdbe89599a2c139153ef0c803
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-01-02 15:36:38 +0100

    Update Definitive.sql

--- Definitive.sql
+++ Definitive.sql
@@ -2426,9 +2426,9 @@ FLUSH PRIVILEGES;
 # 18 TABLES (+ još 5 temporary) (UVJET JE 15)
 # 20 TRIGGERS (NI SPECIFICIRANO U UVJETIMA ALI VJEROJATNO JE 5)
 # 17 REGULAR QUERIES (UVJET JE 15)
-+ 15 FUNCTIONAL QUERIES (ovo ne znači da nan 15 upita od 17 funkcionira, nego da imamo 15 upita povezanih s funkcijama, čisto da ne bude zabune :)) (NEMA TEGA U UVJETIMA)
---------------------------------------------
-= 32 QUERIES IN TOTAL
+# + 15 FUNCTIONAL QUERIES (ovo ne znači da nan 15 upita od 17 funkcionira, nego da imamo 15 upita povezanih s funkcijama, čisto da ne bude zabune :)) (NEMA TEGA U UVJETIMA)
+# --------------------------------------------
+# = 32 QUERIES IN TOTAL
 # 20 VIEWS (5 JE MINIMUM)
 # 15 FUNCTIONS (NEMA TEGA U UVJETIMA, ALI VJEROJATNO JE 5)
 # 31 PROCEDURES + 2 SCHEDULED EVENTS (UVJET JE 10, EVENTE NE SPOMINJU...TO JE ROMEOV PREMIUM TIP :))
